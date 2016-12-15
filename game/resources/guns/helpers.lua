c90cab63bbc8f3bfe5b2b63b57becdace175f1b6
Engin <shultays@gmail.com>
2016-12-16 01:17:30 +0300

    generalized spread effect

--- game/resources/guns/helpers.lua
+++ game/resources/guns/helpers.lua
@@ -1,6 +1,6 @@
 ShootTimer = {}
 
-function ShootTimer.InitGun(gun, interval)
+function ShootTimer.initGun(gun, interval)
 	gun.data.shootInterval = interval
 	gun.data.timeToNextShoot = 0.0
 end
@@ -14,10 +14,44 @@ function ShootTimer.GunShot(gun)
 end
 
 
-function ShootTimer.CheckGun(gun)
+function ShootTimer.checkGun(gun)
 	if gun.data.timeToNextShoot < time and dt > 0.0 then
 		gun.data.timeToNextShoot = time + gun.data.shootInterval * player.shootSpeedMult
 		return true
 	end
 	return false
+end
+
+
+SpreadHelper = {}
+
+function SpreadHelper.initGun(gun)
+	gun.data.timeToNextShoot = time
+	gun.spreadAngle = 0.0
+	gun.data.minSpread = 0.0
+	gun.data.maxSpread = 0.15
+	gun.data.spreadDecreaseStartTime = 0.1
+	gun.data.spreadDecreaseSpeed = 0.15
+	gun.data.spreadIncreasePerShoot = 0.025
+end
+
+function SpreadHelper.onTick(gun)
+	local data = gun.data
+	if data.timeToNextShoot < time - data.spreadDecreaseStartTime then
+		gun.spreadAngle = gun.spreadAngle - dt * data.spreadDecreaseSpeed
+	end
+	
+	if gun.spreadAngle < data.minSpread then
+		gun.spreadAngle = data.minSpread
+	end
+end
+
+
+
+function SpreadHelper.onShoot(gun)
+	local data = gun.data
+	gun.spreadAngle = gun.spreadAngle + data.spreadIncreasePerShoot
+	if gun.spreadAngle > gun.data.maxSpread then
+		gun.spreadAngle = gun.data.maxSpread
+	end
 end
--- game/resources/guns/laser_pistol/laser_pistol.lua
+++ game/resources/guns/laser_pistol/laser_pistol.lua
@@ -2,24 +2,16 @@
 function LaserPistol.init(gun)
 	gun.spreadAngle = 0.0
 	gun.crosshairDistance = 350.0
-	ShootTimer.InitGun(gun, 0.25)
+	ShootTimer.initGun(gun, 0.25)
+	SpreadHelper.initGun(gun)
 end
 
 
 function LaserPistol.onTick(gun)
-	if gun.data.timeToNextShoot < time - 0.1 then
-		gun.spreadAngle = gun.spreadAngle - dt * 0.15
-	end
-	
-	if gun.spreadAngle < 0.0 then
-		gun.spreadAngle = 0
-	end
+	SpreadHelper.onTick(gun)
 	if gun.isTriggered then
-		if ShootTimer.CheckGun(gun) then
-			gun.spreadAngle = gun.spreadAngle + 0.025
-			if gun.spreadAngle > 0.15 then
-				gun.spreadAngle = 0.15
-			end
+		if ShootTimer.checkGun(gun) then
+			SpreadHelper.onShoot(gun)
 			gun:addBullet()
 		end
 	end
