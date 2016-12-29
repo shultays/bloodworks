0ea64c5a99405e06f7d7e4d2d7da8e92b9e9710c
Engin <shultays@gmail.com>
2016-12-29 21:23:08 +0300

    added soloud to project

--- game/main.cpp
+++ game/main.cpp
@@ -22,7 +22,7 @@ Coral coral;
 
 bool Init()
 {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER) < 0)
 	{
 		std::cout << "Failed to init SDL\n";
 		return false;
