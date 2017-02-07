a57bfbce57459d6ba262838410bc879a4cdba570
Engin <shultays@gmail.com>
2017-02-07 23:23:03 +0300

    updated options menu

--- game/cTickBox.h
+++ game/cTickBox.h
@@ -2,13 +2,13 @@
 
 #include "cButton.h"
 
-class cTickbox : public cButton
+class cTickBox : public cButton
 {
 	cTexturedQuadRenderable *checkedRenderable;
 	cTexturedQuadRenderable *uncheckedRenderable;
 	bool checked;
 public:
-	cTickbox(cGame *game, const std::string &checked = "resources/ui/checked_box.png", const std::string &unchecked = "resources/ui/unchecked_box.png", bool isChecked = false);
+	cTickBox(cGame *game, const std::string &checked = "resources/ui/checked_box.png", const std::string &unchecked = "resources/ui/unchecked_box.png", bool isChecked = false);
 	void setChecked(bool isChecked);
 	void check(const Vec2& mousePos);
 	bool isChecked() const
