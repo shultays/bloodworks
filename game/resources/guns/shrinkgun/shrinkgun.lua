70b3d7a0a32fba3e5e904eebc5b2ee9b3dbe2a1a
Engin <shultays@gmail.com>
2017-02-12 23:19:36 +0300

    added shrink gun

---
+++ game/resources/guns/shrinkgun/shrinkgun.lua
@@ -0,0 +1,61 @@
+
+function ShrinkGun.init(gun)
+	gun.spreadAngle = 0.0
+	gun.crosshairDistance = 700.0
+	ShootTimer.initGun(gun, 0.45)
+	
+	SpreadHelper.initGun(gun)
+	gun.data.minSpread = 0.0
+	gun.data.maxSpread = 0.1
+	gun.data.spreadDecreaseSpeed = 0.25
+	gun.data.spreadIncreasePerShoot = 0.025
+end
+
+
+function ShrinkGun.onTick(gun)
+	SpreadHelper.onTick(gun)
+	if gun.isTriggered and gun:hasAmmo() then
+		if ShootTimer.checkGun(gun) then
+			gun:consumeAmmo()
+			SpreadHelper.onShoot(gun)
+			local bullet = gun:addBullet()
+			local particle = bullet:addTrailParticle("CriticalParticle", Vec2.new(0.0, 0.0), 15.0, {})
+			local r = math.random()
+			local g = math.random()
+			local b = math.random()
+			local ra = math.random()
+			if ra < 0.333 then
+				r = 1.0
+			elseif ra < 0.666 then
+				g = 1.0
+			else
+				b = 1.0
+			end
+
+			bullet:setColor(Vec4:new(r, g, b, 1.0))
+		end
+	end
+end
+
+
+
+function ShrinkGun.onBulletHit(gun, bullet, monster)
+	monster:setScale(monster.scale * 0.7)
+	
+	local particleCount = 10
+	if monster.scale < 0.4 then
+		particleCount = 20
+		monster:killSelf(Vec2:new())
+		playSound({path = "resources/sounds/shimmer_1.ogg"})
+	else
+	
+		playSound({path = "resources/sounds/shrink_hit.ogg"})
+	end
+	
+	if monster.data.shrinkParticle == nil then
+		monster.data.shrinkParticle = monster:addParticleSpawner("CriticalParticle", {});
+	end
+	for i=1,particleCount do
+		monster:spawnParticle(monster.data.shrinkParticle, {initialScale = 15.0, moveSpeed = 150.0})
+	end
+end
--- game/resources/particles/critical/script.lua
+++ game/resources/particles/critical/script.lua
@@ -8,8 +8,13 @@ function CriticalParticle.initSystem()
 	addAttribute("uvStart", "vec2")
 end
 
+function CriticalParticle.setDefaultArgs(args)
+	args.moveSpeed = 100.0
+	args.initialScale = 10.0
+	args.rotateSpeed = 6.0
+end
 
-function CriticalParticle.addParticle(params, pos)
+function CriticalParticle.addParticle(params, pos, args)
 	params.pos = Vec2.new(pos.x, pos.y)
 	
 	local r = 1.0
@@ -31,15 +36,14 @@ function CriticalParticle.addParticle(params, pos)
 	
 	params.moveSpeed = Vec2.new(0.0, 0.0)
 	params.moveSpeed:setAngle(math.random() * math.pi * 2.0)
-	params.moveSpeed = params.moveSpeed * (100.0 + math.random() * 50.0)
+	params.moveSpeed = params.moveSpeed * (args.moveSpeed * (1.0 + math.random() * 0.5))
 	
 	if params.bullet ~= nil then
-		params.moveSpeed = params.bullet.moveSpeedDir
+		params.moveSpeed = params.moveSpeed + params.bullet.moveSpeedDir
 	end
 	
-	params.initialScale = 10.0 + math.random() * 10.0
-	
-	params.rotateSpeed = 6.0 + math.random() * 6.0
+	params.initialScale = args.initialScale * (1.0 + math.random())
+	params.rotateSpeed = args.rotateSpeed * (1.0 + math.random())
 	
 	if math.random() > 0.5 then
 		params.rotateSpeed = -params.rotateSpeed
