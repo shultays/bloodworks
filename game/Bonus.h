52338b43011377beb8c1ef0b977e8e6a83a39e56
Engin <shultays@gmail.com>
2017-07-18 23:07:28 +0200

    some bonus chance improvements

--- game/Bonus.h
+++ game/Bonus.h
@@ -3,16 +3,21 @@
 #include <string>
 #include "cTools.h"
 #include "json.h"
+#include "sol.h"
 
 class Bonus
 {
 	std::string name;
 	std::string scriptName;
 	std::string iconPath;
+
+	float spawnChance;
+	sol::function dynamicSpawnChance;
 public:
 	Bonus(nlohmann::json& j);
 	void spawnAt(const Vec2& pos);
 	const std::string& getIconPath() const;
 	const std::string& getName() const;
 	void reset();
+	float getSpawnChance();
 };
