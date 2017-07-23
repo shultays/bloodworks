34f7eb39fda2d195ff873a36302f0589a1cc8942
Engin <shultays@gmail.com>
2017-07-23 03:15:33 +0200

    added server files

---
+++ server/download.php
@@ -0,0 +1,26 @@
+<?php
+ob_start();
+if(isset($_GET['id']))
+{
+// if id is set then get the file with the id from database
+
+include 'opendb.php';
+
+$id    = $_GET['id'];
+$query = "SELECT name, type, size, content " .
+         "FROM upload WHERE id = '$id'";
+
+$result = mysqli_query($link, $query) or die('Error, query failed');
+list($name, $type, $size, $content) = mysqli_fetch_array($result);
+
+header("Content-length: $size");
+header("Content-type: $type");
+header("Content-Disposition: attachment; filename=$name");
+ob_clean();
+flush();
+echo $content;
+
+include 'closedb.php';
+exit;
+}
+?>
