89f9948c4e4289e8bca02bfba1fe47629984ca78
Engin <shultays@gmail.com>
2017-07-23 21:56:07 +0200

    updated server files

--- server/opendb.php
+++ server/opendb.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "mysql.enginmercan.com";
-$username = "bloodworks_db";
-$password = "k2a0631k2NTa!!-";
+$username_db = "bloodworks_db";
+$password_db = "k2a0631k2NTa!!-";
 $dbname = "bloodworks_files";
 
 $link = mysqli_init();
 
 // Create connection
-$conn = mysqli_real_connect($link, $servername, $username, $password, $dbname);
+$conn = mysqli_real_connect($link, $servername, $username_db, $password_db, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
