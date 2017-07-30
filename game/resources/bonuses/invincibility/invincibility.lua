3d84f00e18da45b5a944c2843a341aab50dd0fb4
Engin <shultays@gmail.com>
2017-07-30 20:23:12 +0200

    eh

--- game/resources/bonuses/invincibility/invincibility.lua
+++ game/resources/bonuses/invincibility/invincibility.lua
@@ -10,11 +10,9 @@ function Invincibility.spawn(bonus, pos)
 		Invincibility.data.gameObject = gameObject
 		gameObject:setLevel(RenderableLevel.player + 2)
 		gameObject.data.renderable = gameObject:addTexture(Invincibility.basePath .. "shield.png", "~/resources/default")
-		gameObject.data.renderable.alignment = RenderableAlignment.world
-		gameObject.data.renderable.textureSize = Vec2.new(34.0, 34.0)
-		gameObject.data.renderable.color = 0x4488AAFF
-		gameObject.data.renderable:update()
-		gameObject:setPosition(player.position)
+		gameObject.data.renderable:setAlignment(RenderableAlignment.world)
+		gameObject.data.renderable:setWorldMatrix(Mat3.fromScale(34.0, 34.0))
+		gameObject.data.renderable:setColor(Vec4.fromColor(0x4488AAFF))
 	end
 	bonus:setActive(true)
 end
@@ -24,16 +22,16 @@ function Invincibility.onTick(bonus)
 	local gameObject = data.gameObject
 	data.gameObject:setPosition(player.position)
 	
+		
 	local remaining = data.timeToDie - time
 	if remaining < 2.0 then
 		local t = remaining * 4.0
 		t = t - math.floor(t)
 		if t < 0.5 then
-			gameObject.data.renderable.color = 0x2288AAFF
+			gameObject.data.renderable:setColor(Vec4.fromColor(0x2288AAFF))
 		else
-			gameObject.data.renderable.color = 0x4488AAFF
+			gameObject.data.renderable:setColor(Vec4.fromColor(0x4488AAFF))
 		end
-		gameObject.data.renderable:update()
 	end
 	
 	if remaining < 0.0 then
