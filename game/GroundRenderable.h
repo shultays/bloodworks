5e0ac0893db722b817d32747b433289d24fb43da
Engin <shultays@gmail.com>
2017-08-01 09:15:08 +0200

    added random ground generators

---
+++ game/GroundRenderable.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "cRenderableContainer.h"
+
+class Bloodworks;
+
+class GroundRenderable : public cRenderable
+{
+	GLuint frameBuffer;
+	GLuint frameBufferTexture;
+
+	Bloodworks *bloodworks;
+public:
+	GroundRenderable(Bloodworks *bloodworks);
+	virtual ~GroundRenderable();
+	virtual void render(bool isIdentity, const Mat3& mat, const Rect& crop) override;
+};
