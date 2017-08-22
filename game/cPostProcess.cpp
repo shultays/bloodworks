3e079e90d95d7eb04c6e4b161dae4a1ca7f0d541
Engin <shultays@gmail.com>
2017-08-22 02:47:24 +0200

    added blackhole test code

--- game/cPostProcess.cpp
+++ game/cPostProcess.cpp
@@ -1,6 +1,7 @@
 #include "cPostProcess.h"
 #include "cGame.h"
 #include "cShader.h"
+#include "cGlobals.h"
 
 cPostProcess::~cPostProcess()
 {
@@ -17,11 +18,20 @@ void cPostProcess::init(cGame* game, cShaderShr shader, int level /*= 10000*/)
 	this->game = game;
 	game->addPostProcess(this, level);
 	shaderAmountIndex = addUniformFloat("uShaderAmount", 1.0f);
+
+
+	screenSizeIndex = addUniformVec2("uScreen", game->getScreenDimensions().toVec());
+	timeIndex = addUniformFloat("uTime", timer.getTime());
+	zoomIndex = addUniformFloat("uZoom", game->getCameraZoom());
 }
 
 void cPostProcess::bind()
 {
 	shader->begin();
+
+	setUniform(screenSizeIndex, game->getScreenDimensions().toVec());
+	setUniform(timeIndex, timer.getTime());
+	setUniform(zoomIndex, game->getCameraZoom());
 
 	setShaderUniforms();
 }
