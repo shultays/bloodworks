8366e75f9fbfece41ec81606440bfc0e1e625596
Engin <shultays@gmail.com>
2017-08-31 21:25:01 +0200

    rebuild

--- build.bat
+++ build.bat
@@ -13,4 +13,13 @@ xcopy game\*.exe .\temp\bloodworks /sy
 xcopy game\*.ps .\temp\bloodworks /sy
 xcopy game\*.vs .\temp\bloodworks /sy
 
+del .\temp\bloodworks\config.txt
+del .\temp\bloodworks\user_details.txt
+del .\temp\bloodworks\keys.txt
+
+rmdir .\temp\bloodworks\resources\mods /s /q
+mkdir .\temp\bloodworks\resources\mods
+
+del bloodworks.zip
+
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('temp\bloodworks', 'bloodworks.zip'); }"
