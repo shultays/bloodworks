4810c49eaf0cf934beca9a88dd5c877feebb2999
Engin <shultays@gmail.com>
2017-09-02 21:03:00 +0200

    added report limiters

--- server/send_report.php
+++ server/send_report.php
@@ -6,6 +6,13 @@ if(isset($_POST['report']))
     $message = addslashes($_POST['message']);
     $ip = $_SERVER['REMOTE_ADDR'];
     include 'opendb.php';
+    include 'check_spam.php';
+
+    if (checkSpam($link, 60, 10, "report") == false) {
+        echo "please dont kill my server";
+        include 'closedb.php';
+        exit();
+    }
     $query = "INSERT INTO reports (ip, message, report) VALUES ('$ip', '$message', '$report')";
     if (mysqli_query($link, $query)) 
     {
