e07f42a18cd7768dc137e70648be1c25c2397dd3
Engin <shultays@gmail.com>
2017-09-02 18:21:27 +0200

    added uzi

---
+++ game/resources/guns/uzi/uzi.lua
@@ -0,0 +1,23 @@
+
+function Uzi.init(gun)
+    gun.spreadAngle = 0.15
+    gun.crosshairDistance = 550.0
+    ShootTimer.initGun(gun, 0.02)
+end
+
+
+function Uzi.onTick(gun)
+    if gun.isTriggered and gun:hasAmmo() then
+        if ShootTimer.checkGun(gun) then
+            gun:consumeAmmo()
+            local bullet = gun:addBullet()
+            bullet.onDamageArgs.customSlowdownAmount = 4.0
+            bullet.onDamageArgs.slowdownOnly = true
+            bullet.lifeTime = 0.5 + math.random() * 0.1
+            
+            local particle = bullet:addTrailParticle("BulletTrailParticle", Vec2.new(0.0, 10.0), 15.0, {})
+            particle.args.initialScale = 1.5
+            particle.args.fadeOutSpeed = 1.8
+        end
+    end
+end
