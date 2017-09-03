c0bd1757dc1bd36309e6277b4b42dd4e6380cff3
Engin <shultays@gmail.com>
2017-09-03 17:59:14 +0200

    custom game support

---
+++ game/source/CustomGameWindow.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "cGlobals.h"
+
+class Bloodworks;
+class cRenderableContainer;
+class cTextRenderable;
+class cScrollContainer;
+class cButton;
+
+class CustomGameWindow
+{
+	Bloodworks *bloodworks;
+
+	cRenderableContainer *window;
+	cTextRenderable *title;
+	cTextRenderable *currentDescription;
+
+	cScrollContainer *buttonContainer;
+	cVector<cButton*> customGameButtons;
+	cVector<std::string> customGameScripts;
+	cVector<std::string> customGameNames;
+	cVector<std::string> customGameDescriptions;
+
+	void clearButtons();
+	void loadMods();
+
+public:
+	CustomGameWindow(Bloodworks *bloodworks);
+	~CustomGameWindow();
+	bool isVisible() const;
+	void show();
+	void tick();
+};
