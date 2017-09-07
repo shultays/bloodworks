16b702a901afc2e1cef6108bc14664a334c308ea
Engin <shultays@gmail.com>
2017-09-07 05:45:31 +0200

    updated git ignore

---
+++ server/opendb.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "";
+$username_db = "";
+$password_db = "";
+$dbname = "";
+$reportPass = "";
+
+$link = mysqli_init();
+
+// Create connection
+$conn = mysqli_real_connect($link, $servername, $username_db, $password_db, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
