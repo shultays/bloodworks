1603b7d75517abee94ef42b5ae07626debce39f4
Engin <shultays@gmail.com>
2017-09-08 22:41:57 +0200

    report fixes

--- server/send_report.php
+++ server/send_report.php
@@ -3,6 +3,14 @@ $success = "error";
 
 include_once 'hasher.php';
 
+$isfeedback = false;
+$max  = 10;
+if($_SERVER['HTTP_REFERER'])
+{
+    $isfeedback = true;
+    $max  = 0;
+}
+
 if(isset($_POST['report']))
 {
     $ip = hashIP($_SERVER['REMOTE_ADDR']);
@@ -12,12 +20,12 @@ if(isset($_POST['report']))
     $report = $link->real_escape_string($_POST['report']);
     $message = $link->real_escape_string($_POST['message']);
     
-    if (checkSpam($link, 60, 10, "report") == false) {
-        echo "please dont kill my server";
+    if (checkSpam($link, 60, $max, "report") == false) {
+        echo "Please wait before sending new reports.";
         include 'closedb.php';
         exit();
     }
-    $query = "INSERT INTO reports (ip, message, report) VALUES ('$ip', '$message', '$report')";
+    $query = "INSERT INTO reports (ip, message, report, feedback) VALUES ('$ip', '$message', '$report', '$isfeedback')";
     if (mysqli_query($link, $query)) 
     {
         $success = "Report has been received. Thanks!";
