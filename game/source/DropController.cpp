4b973450716bc86bc0e0e3314e0f6537baf66bf3
Engin <shultays@gmail.com>
2017-09-09 00:36:01 +0200

    improved drıos

--- game/source/DropController.cpp
+++ game/source/DropController.cpp
@@ -102,7 +102,7 @@ void DropController::tick()
 	if (timer.getDt() > 0.0f)
 	{
 		float timeSinceLastDrop = timer.getTime() - lastRandomDropSpawn;
-		float extraDropChance = (timeSinceLastDrop - 10.0f) / 30.0f;
+		float extraDropChance = (timeSinceLastDrop - 10.0f) / 20.0f;
 		float r = randFloat();
 		if (r < extraDropChance)
 		{
@@ -254,7 +254,7 @@ void DropController::onMonsterDied(Monster* monster, float dropChance)
 			return;
 		}
 
-		float extraDropChance = (timeSinceLastDrop - 10.0f) / 50.0f;
+		float extraDropChance = (timeSinceLastDrop - 10.0f) / 30.0f;
 		float r = randFloat();
 		if (r < dropChance + extraDropChance)
 		{
