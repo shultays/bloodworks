e8fa4cb0637e7086ef65354b099aa1eb3387211c
Engin <shultays@gmail.com>
2017-12-16 17:16:09 +0100

    balancing

--- game/resources/perks/explosion_on_reload/explosion_on_reload.lua
+++ game/resources/perks/explosion_on_reload/explosion_on_reload.lua
@@ -7,6 +7,6 @@ end
 function ExplosionOnReload.onReload(gun)
     if gun:hasAmmo() == false then
         addExplosion(player.position, 100.0, 200.0, 80, 140)
-    playSound({path = "~/resources/sounds/explode.ogg", volume = 0.5})
+        playSound({path = "~/resources/sounds/explode.ogg", volume = 0.5})
     end
 end
